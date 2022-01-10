02823b793022122d950fadb29e530a1be6c6c7c3
Jimmy Y <jimmy@reslv.io>
2022-01-10 13:06:23 +0800

    start project

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
